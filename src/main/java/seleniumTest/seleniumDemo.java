05310c0ff44b9300ba4f856e3d928e00c21a80d6
Joshua Pontipiedra <kentaris@gmail.com>
2025-05-29 18:03:41 -0400

    Added test for Add/Remove button page

--- src/main/java/seleniumTest/seleniumDemo.java
+++ src/main/java/seleniumTest/seleniumDemo.java
@@ -7,39 +7,80 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class seleniumDemo {
 
-	public static void main(String[] args) {
-
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	public void openBrowser() {
+		
 		//class object
 		ChromeOptions co = new ChromeOptions();
-		
+				
 		//Setting browser version
 		co.setBrowserVersion("137");
-		
+			
 		//Creating new WebDriver - Chrome
-		WebDriver driver = new ChromeDriver();
-		
+		driver = new ChromeDriver();
+				
 		//maximizing window
 		driver.manage().window().maximize();
-		
+				
 		//Setting page to open
-		driver.get("https://the-internet.herokuapp.com/inputs");
+		driver.get("https://the-internet.herokuapp.com");
 		
-		//looking for search bar (google)
-		//WebElement element = driver.findElement(By.name("q"));
-		//WebElement element = driver.findElement(By.className("gLFyf"));
-		
-		//WebElement element = driver.findElement(By.className("example"));
-		WebElement element = driver.findElement(By.xpath("//div[@class='example']/input[@type='number']"));
-		
-		//searching then submitting
-		element.sendKeys("20");
-		//element.submit();
-		
-		//Close Browser
-		//driver.close();
+		wait = new WebDriverWait (driver,Duration.ofSeconds(5));
 	}
+	public void addRemoveElements() throws InterruptedException {
+		
+		WebElement link = driver.findElement(By.linkText("Add/Remove Elements"));
+		
+		link.click();
+		
+		WebElement addButton = driver.findElement(By.xpath("//div[@class='example']/button[@onclick='addElement()']"));
+		addButton.click();
+		
+		//Adding sleep timer to slow test down a bit
+		Thread.sleep(2000);
+		
+		WebElement remButton = driver.findElement(By.className("added-manually"));
+		remButton.click();
+		
+		Thread.sleep(2000);
+		
+		//Going back a page
+		driver.navigate().back();
+		
+		
+	}
+	
+	public void inputPage() {
+		
+		//Calling new class - Inputs
+		WebElement link = driver.findElement(By.linkText("Inputs"));
+		
+		link.click();
+		
+		//wait = new WebDriverWait (driver, Duration.ofSeconds(10));
+				
+		WebElement textInput = driver.findElement(By.xpath("//div[@class='example']/input[@type='number']"));
+				
+		//searching then submitting
+		textInput.sendKeys("20");
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+
+		
+		
+		seleniumDemo obj = new seleniumDemo();
+		obj.openBrowser();
+		obj.addRemoveElements();
+		obj.inputPage();
+		
+	}
+
 
 }
